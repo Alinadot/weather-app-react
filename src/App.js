f779a4824ff71403aa6bfb2618acf73a4dd23c2a
Alinadot <71038633+Alinadot@users.noreply.github.com>
2020-12-03 20:26:36 +0100

    added the github url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-    Hello weather
+    Hello weather!
+    <footer>
+      This project is created by Alina Shamko and is{" "}
+  <a href="https://github.com/Alinadot/weather-app-react"targe="_blank">open-sourced on GitHub</a>
+    </footer>
     </div>
   );
 }
