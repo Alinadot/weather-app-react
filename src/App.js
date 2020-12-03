8e8de512fecab3c03c347c23d66daa663b5659e4
Alinadot <71038633+Alinadot@users.noreply.github.com>
2020-12-03 21:25:14 +0100

    rel="noreferrer"

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-  
+   
    <Weather />
     <footer>
       This project is coded by Alina Shamko and is{" "}
-  <a href="https://github.com/Alinadot/weather-app-react"target="_blank">open-sourced on GitHub</a>
+  <a href="https://github.com/Alinadot/weather-app-react"target="_blank" rel="noreferrer">open-sourced on GitHub</a>
     
     </footer>
     </div>
