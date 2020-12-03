2127c247f27c42965ada9ca567feb62c727732a6
Alinadot <71038633+Alinadot@users.noreply.github.com>
2020-12-03 20:48:30 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-    Hello weather!
+      <div className="container">
+   <h1> Hello weather!</h1>
     <footer>
-      This project is created by Alina Shamko and is{" "}
-  <a href="https://github.com/Alinadot/weather-app-react"targe="_blank">open-sourced on GitHub</a>
+      This project is coded by Alina Shamko and is{" "}
+  <a href="https://github.com/Alinadot/weather-app-react"target="_blank">open-sourced on GitHub</a>
+    
     </footer>
+    </div>
     </div>
   );
 }
