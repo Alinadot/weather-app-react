a949466c36e81b8bc2de47556c74691a1a915a1b
Alinadot <71038633+Alinadot@users.noreply.github.com>
2020-12-06 16:53:45 +0100

    css weather update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
    <Weather />
     <footer>
       This project is coded by Alina Shamko and is{" "}
-  <a href="https://github.com/Alinadot/weather-app-react"target="_blank" rel="noreferrer">open-sourced on GitHub</a>
+  <a href="https://github.com/Alinadot/weather-app-react"target="_blank" rel="noreferrer" >open-sourced on GitHub</a>
     
     </footer>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -6,10 +6,10 @@ return <div className="Weather">
     <form>
       <div className="row">
       <div className="col-9">  
-        <input type="search" placeholder="Type a city..." className="form-control"/>
+        <input type="search" placeholder="Type a city..." className="form-control"autoFocus="on" />
         </div> 
         <div className="col-3">
-        <input type="submit" value="Search" className="btn btn-primary" />
+        <input type="submit" value="Search" className="btn btn-warning" />
    </div>
     </div>
     </form>
@@ -18,12 +18,18 @@ return <div className="Weather">
         <li>Thursday 00:00 </li>
         <li> Rainy </li>
     </ul>
-    <div className="row">
+ 
+    <div className="row mt-3">
        <div className="col-6">
- <img src="https://ssl.gstatic.com/onebox/weather/64/rain.png" alt="Rainy"/>
-       3°C
-       </div> 
-      <div className="col-6">
+        <div className="clearfix">
+        <img src="https://ssl.gstatic.com/onebox/weather/64/rain.png" alt="Rainy" className="float-left"/>
+      <div className="float-left">
+      <span className="temperature">3</span> 
+      <span className="unit">°C</span>
+      </div>
+       </div>
+</div> 
+     <div className="col-6">
       <ul>
 <li> 
     Percipitation:94%
