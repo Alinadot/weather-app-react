ca7c47bd2ff11202379cb34c9ba324b7d4c2a1f0
Alinadot <71038633+Alinadot@users.noreply.github.com>
2020-12-10 18:05:36 +0100

    changed { to [ in unit :/

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 
 
 export default function WeatherTemperature (props) {
-const {unit, setUnit} = useState ("celsius");
+const [unit, setUnit] = useState ("celsius");
 
 if (unit === "celsius") {
     return  (
         <div>
            <span className="temperature">{Math.round(props.celsius)}</span> 
-      <span className="unit">°C</span>
+      <span className="unit">°C | <a href="/"> F</a></span>
       </div>
     ); 
 }
